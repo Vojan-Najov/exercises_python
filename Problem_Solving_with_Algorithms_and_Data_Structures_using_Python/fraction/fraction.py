d221f4416370156be79c906ab022f1776c912628
vojan <serejah.mashichev@gmail.com>
2024-10-07 02:29:01 +0300

    Add some exercises for the 'Fraction' sample to PSwAaDSuP

--- Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/fraction/fraction.py
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/fraction/fraction.py
@@ -10,42 +10,52 @@ class Fraction:
 
     def __init__(self, top, bottom):
 
-        self.num = top
-        self.den = bottom
+        if not type(top) is int:
+            raise RunTimeError(f"Error: top {top} not an integer.")
+        if not type(bottom) is int:
+            raise RunTimeError(f"Error: bottom {bottom} not an integer.")
+
+        if bottom < 0:
+            top, bottom = -top, -bottom
+        common = gcd(top, bottom)
+        self.num = top // common
+        self.den = bottom // common
+
+    def getNum(self):
+
+        return self.num
+
+    def getDen(self):
+
+        return self.den
 
     def __str__(self):
 
         return str(self.num) + "/" + str(self.den)
 
     def __add__(self, other):
+
         new_num = self.num * other.den + other.num * self.den
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __sub__(self, other):
+
         new_num = self.num * other.den - other.num * self.den
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __mul__(self, other):
 
         new_num = self.num * other.num
         new_den = self.den * other.den
-        common = gcd(new_num, new_den)
-
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __truediv__(self, other):
 
         new_num = self.num * other.den
         new_den = self.den * other.num
-        common = gcd(new_num, new_den)
-
-        return Fraction(new_num // common, new_den // common)
+        return Fraction(new_num, new_den)
 
     def __eq__(self, other):
 
@@ -54,10 +64,22 @@ class Fraction:
     def __lt__(self, other):
 
         return self.num * other.den < other.num * self.den
+
+    def __le__(self, other):
+
+        return self < other or self == other
     
     def __gt__(self, other):
 
         return self.num * other.den > other.num * self.den
+
+    def __ge__(self, other):
+
+        return self > other or self == other
+
+    def __ne__(self, other):
+
+        return not self == other
 
 
 
@@ -108,5 +130,54 @@ def main():
     print(f"{f2} < {f1} is {f2 < f1}")
     print(f"{f1} > {f2} is {f1 > f2}")
     print(f"{f2} > {f1} is {f2 > f1}")
+    print()
+
+    f = Fraction(2, 4)
+    print(f"num is {f.getNum()} and den is {f.getDen()}")
+    print()
+
+    f1 = Fraction(1, 2)
+    f2 = Fraction(2, 4)
+    f3 = f1 + f2
+    print(f"{f1} + {f2} = {f3}")
+    print()
+
+    f1 = Fraction(1, 2)
+    f2 = Fraction(3, 6)
+    f3 = Fraction(2, 3)
+    print(f"{f1} <= {f2} is {f1 <= f2}")
+    print(f"{f1} <= {f3} is {f1 <= f3}")
+    print(f"{f3} <= {f1} is {f3 <= f1}")
+    print()
+
+    f1 = Fraction(1, 2)
+    f2 = Fraction(3, 6)
+    f3 = Fraction(2, 3)
+    print(f"{f1} >= {f2} is {f1 >= f2}")
+    print(f"{f1} >= {f3} is {f1 >= f3}")
+    print(f"{f3} >= {f1} is {f3 >= f1}")
+    print()
+
+    f1 = Fraction(1, 2)
+    f2 = Fraction(3, 6)
+    f3 = Fraction(2, 3)
+    print(f"{f1} != {f2} is {f1 != f2}")
+    print(f"{f1} != {f3} is {f1 != f3}")
+    print(f"{f3} != {f1} is {f3 != f1}")
+    print()
+
+    try:
+        f = Fraction(12.3, 4)
+    except:
+        print("Ok")
+    try:
+        f = Fraction(12, 4.2)
+    except:
+        print("Ok")
+    print()
+
+    f = Fraction(2, -10)
+    print(f)
+    print()
 
 main()
