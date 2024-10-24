0ad469e0b46c180937c593a9b381d883c6fad97a
vojan <serejah.mashichev@gmail.com>
2024-10-25 00:45:29 +0300

    fix sample 'parchecker' to 'PSwAaDSup'

--- Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/parchecker/parchecker.py
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/parchecker/parchecker.py
@@ -8,17 +8,27 @@ def parChecker(symbolString):
     index = 0
     while index < len(symbolString) and balanced:
         symbol = symbolString[index]
-        if symbol == "(":
+        if symbol in "([{":
             s.push(symbol)
-        elif symbol == ")":
+        elif symbol in ")]}":
             if s.isEmpty():
                 balanced = False
             else:
-                s.pop()
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
         index += 1
 
     return balanced and s.isEmpty()
 
+def matches(open, close):
+    opens = ('(', '[', '{', )
+    closers = (')', ']', '}', )
+    return opens.index(open) == closers.index(close)
+
 if __name__ == "__main__":
     print(parChecker('(((1 + 2))())'))
     print(parChecker('(()'))
+    
+    print(parChecker('{{([][1, 2])}()}'))
+    print(parChecker('[{()]'))
