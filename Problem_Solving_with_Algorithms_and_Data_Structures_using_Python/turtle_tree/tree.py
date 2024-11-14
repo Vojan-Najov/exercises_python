4527d3a0e46f885516bb31d430d05fdd856219bb
vojan <serejah.mashichev@gmail.com>
2024-11-14 17:33:41 +0300

    Add sample 'turtle_tree' to 'PSwAaDSup'

---
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/turtle_tree/tree.py
@@ -0,0 +1,25 @@
+import turtle
+
+def tree(branchlen, t):
+    if branchlen > 5:
+        t.forward(branchlen)
+        t.right(20)
+        tree(branchlen - 15, t)
+        t.left(40)
+        tree(branchlen - 15, t)
+        t.right(20)
+        t.backward(branchlen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75, t)
+    myWin.exitonclick()
+
+main()
+        
