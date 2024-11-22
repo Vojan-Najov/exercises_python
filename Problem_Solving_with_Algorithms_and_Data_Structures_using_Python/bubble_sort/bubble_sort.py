7f754efb03ea82a2a7a0a1d2a3b209d7250e8c23
vojan <serejah.mashichev@gmail.com>
2024-11-22 20:16:42 +0300

    Add sample 'bubble_sort' to 'PSwAaDSup'

---
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/bubble_sort/bubble_sort.py
@@ -0,0 +1,12 @@
+
+def bubble_sort(alist):
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
+
+if __name__ == '__main__':
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(f'Before: {alist}')
+    bubble_sort(alist)
+    print(f' After: {alist}')
