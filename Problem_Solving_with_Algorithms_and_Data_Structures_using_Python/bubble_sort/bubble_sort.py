03a3c8b73a4e78b50643610710221e3c68cd37a3
vojan <serejah.mashichev@gmail.com>
2024-11-23 16:04:45 +0300

    Add short bubble sort sample 'bubble_sort' to 'PSwAaDSup'

--- Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/bubble_sort/bubble_sort.py
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/bubble_sort/bubble_sort.py
@@ -5,8 +5,26 @@ def bubble_sort(alist):
             if alist[i] > alist[i + 1]:
                 alist[i], alist[i + 1] = alist[i + 1], alist[i]
 
+def short_bubble_sort(alist):
+    exchanges = True
+    passnum = len(alist) - 1
+
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                exchanges = True
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
+        passnum -= 1
+
+
 if __name__ == '__main__':
     alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print(f'Before: {alist}')
     bubble_sort(alist)
+    print(f' After: {alist}')
+
+    alist = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+    print(f'Before: {alist}')
+    short_bubble_sort(alist)
     print(f' After: {alist}')
