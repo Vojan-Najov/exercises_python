abf25f9b85726d7f7f4a5595ec5adecfa18a6b1a
vojan <serejah.mashichev@gmail.com>
2024-11-27 12:45:57 +0300

    Add 5.1.G to 'yandex_handbook_basic'

--- yandex_handbook_basic/5.1.G.py
+++ yandex_handbook_basic/5.1.G.py
@@ -52,54 +52,59 @@ def rounder(prec=0):
 class Rectangle():
 
     def __init__(self, point1, point2):
-        self.point = min(point1[0], point2[0]), max(point1[1], point2[1])
         self.width = abs(point1[0] - point2[0])
         self.height = abs(point1[1] - point2[1])
+        self.centre = \
+                min(point1[0], point2[0]) + self.width / 2, \
+                max(point1[1], point2[1]) - self.height / 2
 
     @rounder(2)
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        size = self.get_size()
+        return 2 * (size[0] + size[1])
 
     @rounder(2)
     def area(self):
-        return self.width * self.height
+        size = self.get_size()
+        return size[0] * size[1]
 
     def get_pos(self):
-        return round(self.point[0], 2), round(self.point[1], 2)
+        return round(self.centre[0] - self.width / 2, 2), round(self.centre[1] + self.height / 2, 2)
 
     def get_size(self):
         return round(self.width, 2), round(self.height, 2)
 
     def move(self, dx, dy):
-        self.point = (self.point[0] + dx, self.point[1] + dy)
+        self.centre = (self.centre[0] + dx, self.centre[1] + dy)
 
     def resize(self, width, height):
         self.width = width
         self.height = height
+        self.centre = (self.point[0] + width / 2, self.point[1] - height / 2)
 
     def turn(self):
-        centre = self.point[0] + self.width / 2, self.point[1] - self.height / 2
         self.width, self.height = self.height, self.width
-        self.point = centre[0] - self.width, centre[1] - self.height
+
+    def scale(self, factor):
+        self.width *= factor
+        self.height *= factor
+        self.width = round(self.width, 2)
+        self.height = round(self.height, 2)
 
 
 if __name__  == '__main__':
 
     rect = Rectangle((3.14, 2.71), (-3.14, -2.71))
+    print(rect.get_pos(), rect.get_size(), sep='\n')
     assert (rect.get_pos(), rect.get_size()) == ((-3.14, 2.71), (6.28, 5.42))
-# rect.turn()
+
+    rect.turn()
+    assert (rect.get_pos(), rect.get_size()) == ((-2.71, 3.14), (5.42, 6.28))
+
+    rect = Rectangle((3.14, 2.71), (-3.14, -2.71))
+    assert (rect.get_pos(), rect.get_size()) == ((-3.14, 2.71), (6.28, 5.42))
+
+    rect.scale(2.0)
+    assert (rect.get_pos(), rect.get_size()) == ((-6.28, 5.42), (12.56, 10.84))
 # print(rect.get_pos(), rect.get_size(), sep='\n')
 # Вывод
-# (-2.71, 3.14)
-# (5.42, 6.28)
-# Пример 2
-# Ввод
-# rect = Rectangle((3.14, 2.71), (-3.14, -2.71))
-# print(rect.get_pos(), rect.get_size(), sep='\n')
-# rect.scale(2.0)
-# print(rect.get_pos(), rect.get_size(), sep='\n')
-# Вывод
-# (-3.14, 2.71)
-# (6.28, 5.42)
-# (-6.28, 5.42)
-# (12.56, 10.84)
