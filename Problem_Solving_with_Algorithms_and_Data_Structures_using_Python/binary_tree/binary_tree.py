7b90ca50098572c3e3c4438577171cc88d9ca8af
vojan <serejah.mashichev@gmail.com>
2024-12-02 14:08:37 +0300

    Expand sample 'binary_tree' to 'PSwAaDSup'

--- Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/binary_tree/binary_tree.py
+++ Problem_Solving_with_Algorithms_and_Data_Structures_using_Python/binary_tree/binary_tree.py
@@ -35,6 +35,27 @@ class BinaryTree:
         return self.key
 
 
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+
+def postorder(tree):
+    if tree:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+
+def inorder(tree):
+    if tree:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
+
 def main():
     r = BinaryTree('a')
     print(r.getRootVal())
